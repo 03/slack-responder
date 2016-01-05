b6f52c8a60d4d4ad2edf9f36f99c6cc19bb763a0
Luke <feng.oz@hotmail.com>
2016-01-05 15:07:57 +1100

    Update slack_commands_controller.rb

--- app/controllers/slack_commands_controller.rb
+++ app/controllers/slack_commands_controller.rb
@@ -5,15 +5,32 @@ class SlackCommandsController < ApplicationController
   WHITELIST_TOKENS = [
     ENV["SLACK_WORK_COMMAND_TOKEN"],
     ENV["SLACK_CARD_COMMAND_TOKEN"],
-    ENV["SLACK_RETRO_COMMAND_TOKEN"]
+    ENV["SLACK_RETRO_COMMAND_TOKEN"],
+    ENV["SLACK_CHAT_COMMAND_TOKEN"]
   ].compact
 
-  def work
+  def chat
+    # merge username into the topic
+    params[:text] += " <" + params[:user_name] + ">"
     response = SlackTrello::Commands::Work.new(params, ENV["SLACK_WEBHOOK_URL"]).run
+    render text: response
+  end
+  
+  def work
+    # merge username into the topic
+    params[:text] += " <" + params[:user_name] + ">"
+    # e.g: /card (general today) what is new
+    # /work (list_name)##(content)
+    params[:text] = "(" params[:channel_name] + " " + params[:text].split("##")[0].strip + ")" + params[:text].split("##")[1].strip
+    puts params[:text].inspect
+    #response = SlackTrello::Commands::Work.new(params, ENV["SLACK_WEBHOOK_URL"]).run
+    response = SlackTrello::Commands::CreateCard.new(params, ENV["SLACK_WEBHOOK_URL"]).run
     render text: response
   end
 
   def create_card
+    # merge username into the topic
+    params[:text] += " <" + params[:user_name] + ">"
     response = SlackTrello::Commands::CreateCard.new(params, ENV["SLACK_WEBHOOK_URL"]).run
     render text: response
   end
@@ -42,7 +59,7 @@ class SlackCommandsController < ApplicationController
   private
 
   def slash_command_auth
-    puts 'Hello Slash!!'
+    puts 'Hello Sensei!!'
     puts WHITELIST_TOKENS.inspect
     unless WHITELIST_TOKENS.include?(params[:token])
       render text: "Unauthorized", status: :unauthorized
