45eb1eae058e4f3d185d2a55fb83aa75b5e37cda
Luke <feng.oz@hotmail.com>
2016-01-08 11:15:25 +1100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   post "/slack/card", :to => "slack_commands#create_card"
   post "/slack/copy_cards", :to => "slack_commands#copy_cards"
   
-  get 'health' => :to => 'health#index'
+  get 'health'
 end
