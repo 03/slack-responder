0313ae22d257a80889b7dc98d4365972d0b662e8
Luke <feng.oz@hotmail.com>
2016-01-08 14:01:02 +1100

    Update health_controller.rb

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,7 +1,7 @@
 class HealthController <  ApplicationController
   def index
     puts 'HELLO'
-    #@text_to_check = 'Slack-Responder-OK'
-    #@timestamp = Time.now
+    @text_to_check = 'Slack-Responder-OK'
+    @timestamp = Time.now
   end
 end
