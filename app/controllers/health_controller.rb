58aa0591e497d7b59f1619d280bf529da923a92b
Luke <feng.oz@hotmail.com>
2016-01-08 15:52:12 +1100

    Update health_controller.rb

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,9 +1,5 @@
 class HealthController <  ActionController::Base
   def index
-    puts 'HELLO 2'
-    @text_to_check = 'Slack-Responder-OK'
-    @timestamp = Time.now
-    
-    render text: @text_to_check
+    render text: Time.now.inspect + ' --> Slack-Responder-OK'
   end
 end
